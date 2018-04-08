1eaed0897678fe75764f8288e18942652fd01360
Rahul Ghosh <ghrahul2016@gmail.com>
2018-04-08 22:15:49 +0530

    adding list items to the day2 and day3.....and trying to work on listviewitemclicklistener

--- app/src/main/java/org/weibeld/example/tabs/Page3Fragment.java
+++ app/src/main/java/org/weibeld/example/tabs/Page3Fragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.weibeld.example.R;
 
@@ -14,6 +16,25 @@ public class Page3Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_page3, container, false);
+        String[] day3array = {
+                "Debate",
+                "Music",
+                "Busketball",
+                "Cricket",
+                "Football",
+                "Badminton",
+                "Debate",
+                "Music",
+                "Busketball",
+                "Cricket",
+                "Football",
+                "Badminton"
+
+        };
+        ListView lv3 = (ListView)rootView.findViewById(R.id.day3list);
+        ArrayAdapter<String> lvc = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, day3array);
+        lv3.setAdapter(lvc);
         return rootView;
     }
 }
