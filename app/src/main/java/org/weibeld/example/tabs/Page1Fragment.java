c0dfa181c0d355f74013e75aa1374f38240e48e3
Rahul Ghosh <ghrahul2016@gmail.com>
2018-04-10 02:11:26 +0530

    adding list items to the day2 and day3.....and trying to work on listviewitemclicklistener

--- app/src/main/java/org/weibeld/example/tabs/Page1Fragment.java
+++ app/src/main/java/org/weibeld/example/tabs/Page1Fragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -22,18 +23,42 @@ public class Page1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_page1, container, false);
         String[] day1array = {
-          "Debate",
-           "Music",
-            "Busketball",
-             "Cricket",
-                "Football"
+                "Debate",
+                "Music",
+                "Busketball",
+                "Cricket",
+                "Football",
+                "Badminton"
 
         };
         ListView lv1 = (ListView)rootView.findViewById(R.id.day1list);
         ArrayAdapter<String> lva = new ArrayAdapter<String>(
                 getActivity(), android.R.layout.simple_list_item_1, day1array);
-        lv1.setAdapter(lva);
+        /*lv1.setAdapter(lva);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), Debate.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), Music.class);
+                    startActivityForResult(myIntent, 1);
+                }
+
+
+
+            }
+        });*/
+
 
         return rootView;
+
+
     }
+
+
 }
--- app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
+++ app/src/main/java/org/weibeld/example/tabs/Page2Fragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.weibeld.example.R;
 
@@ -14,6 +16,21 @@ public class Page2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_page2, container, false);
+        String[] day2array = {
+                "Debate",
+                "Music",
+                "Busketball",
+                "Cricket",
+                "Football",
+                "Badminton"
+
+        };
+        ListView lv2 = (ListView)rootView.findViewById(R.id.day2list);
+        ArrayAdapter<String> lvb = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, day2array);
+        lv2.setAdapter(lvb);
+
+
         return rootView;
     }
 
